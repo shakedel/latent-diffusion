f2a66f9e3a7f73eee932dd6ce465da339ae0d812
Elad Shaked <elad.shaked@dataspan.ai>
2022-12-30 21:49:03 +0200

    Fixed setup.py to include source dirs

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+packages = ['latent_diffusion', 'latent_diffusion.scripts', 'latent_diffusion.ldm'] + ['latent_diffusion.ldm.'+p for p in find_packages('ldm')]
+
 setup(
     name='latent-diffusion',
     version='0.0.1',
     description='',
-    packages=find_packages(),
+    packages=packages,
+    package_dir={p: p.replace('.', '/').replace('latent_diffusion', '.') for p in packages},
     install_requires=[
         'torch',
         'numpy',
